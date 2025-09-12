ed91bc8a6b408b5c17ec64cb701f133a3eb0c763
Mina Boles Gergis <minaboles96@gmail.com>
2025-09-12 20:47:35 +0300

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-age = int(input("how old are you : "))
+age = int(input("How old are you?  "))
 calc_expect_Age = age + 27
 print("In 2050, you will be ", calc_expect_Age, "years old.")
 # the Age = 27
