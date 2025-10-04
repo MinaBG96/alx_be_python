fc19d3f079e435ef3677deab7aa4ec3269dd4c8b
MinaBG96 <minaboles96@gmail.com>
2025-10-04 19:50:31 +0300

    Arithmetic Operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1: float ,num2: float,operation: str):
+    if operation == "add":
+        return (num1 + num2)
+    
+    if operation == "subtract":
+        return (num1 - num2)
+    
+    if operation == "multiply":
+        return (num1 * num2)
+    
+    if operation == "divide":
+        if num2 == 0:
+            print("can't divide by zero ")
+        else:
+            return (num1 / num2)
+    
