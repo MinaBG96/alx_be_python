71b511b5226d36d9f5cc4115ce3929395d66c854
MinaBG96 <minaboles96@gmail.com>
2025-10-04 21:04:06 +0300

    Shopping List App

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,39 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice  = input("Enter your choice: ")
+        
+        if choice == "1":
+            print(shopping_list)
+            number = int(input("Enter the number you want to add : "))
+            shopping_list.append(number)
+            print(shopping_list)
+            print("the number add successfully")
+        
+        elif choice == "2":
+            print(shopping_list)
+            number = int(input("Enter the number you want to remove: "))
+            shopping_list.remove(number)
+            print(shopping_list)
+            print("the number removed successfully")
+            
+        elif choice == "3":
+            print(shopping_list)
+            
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+if __name__ == "__main__":
+    main()
