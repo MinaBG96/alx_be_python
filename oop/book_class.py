08ff0ca4bca4ae1002822d298b315c9b61467bf3
MinaBG96 <minaboles96@gmail.com>
2025-10-12 23:58:31 +0300

    book class task v1.0

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        print(f"{self.title}, by {self.author}, published in {self.year}\t")
+        return "(title) by (author), published in (year)"
+
+    def __repr__(self):
+        print()
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+
+    def __del__(self):
+        print("Deleting (title of the book)")
+        print(self.title)
